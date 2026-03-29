aa681a0e53ac5872d745c42ebf3f714ae2afa48d
JackAnsh23 <raiansh230405@gmail.com>
2026-03-29 22:34:44 +0530

    coding main navbar header file

---
+++ components/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+
+
+const Header = () => {
+  return (
+    <header>
+        <div className="main-container inner">
+        <Link href="/">
+          <Image src="/logo.svg" alt="CoinPulse logo" width={132} height={40} />
+        </Link>
+        </div>
+    </header>
+  )
+}
+
+export default Header
