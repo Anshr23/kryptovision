99615be39ecdb9d1e1e136f85fe0e71701f3c3a1
JackAnsh23 <raiansh230405@gmail.com>
2026-03-30 22:34:44 +0530

    datatable and header on page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import DataTable from "@/components/DataTable"
+import Image from "next/image"
 
 const page = () => {
   return (
-    <p className='text-3xl text-indigo-500'>Coin Plus</p>
+    <main className="main-container">
+      <section className="home-grid">
+        <div className="coin-overview">
+          <div className="header pt-2">
+            <Image src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png" 
+            width={56} height={56} alt="Bitcoin's Logo" />
+
+            <div className="info">
+              <p>Bitcoin / BTC</p>
+              <h1>$81,534.55</h1>
+            </div>
+          </div>
+        </div>
+
+        <p>Trending Coins</p>
+        <DataTable />
+      </section>
+
+      <section className="w-full mt-7 space-y-4">
+        <p>Categories</p>
+      </section>
+    </main>
   )
 }
 
